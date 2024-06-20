686a1e4a83921f5b8f7a312bc8198edfea412f5f
LChebetcodes <lchebet0@gmail.com>
2024-06-20 09:01:31 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-int main()
+/**
+ * main - A program that printsa line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
 {
-	char Mystr[] = ""Programming is like building a multilingual puzzle";
-	puts(Mystr);
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
