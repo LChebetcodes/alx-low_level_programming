58f2b37f0aef07fce86694810db67dd6073cf31d
LChebetcodes <lchebet0@gmail.com>
2024-06-20 11:08:27 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints size of various computer types
+ * main - A  c program that prints size of various computer types
  * return: 0 (Success)
  */
 int main(void)
