a49038b037de410464c329df1aea04fd614ba72f
LChebetcodes <lchebet0@gmail.com>
2024-06-21 10:49:57 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -A c program that prints the alphabet in lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar('ch')
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar('ch');
+	putchar('\n');
+	return (0);
+}
+
+
